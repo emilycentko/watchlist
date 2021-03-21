4d875230efd2d008a1c18cc88b7a02b56e4bdd91
emilycentko <emily.centko@gmail.com>
2021-03-20 19:22:02 -0500

    Edited height of movie poster in search filter and fixed issue to set state to empty array when nothing in search field to not give an error message on DOM

--- src/components/movies/MovieSearch.js
+++ src/components/movies/MovieSearch.js
@@ -2,18 +2,23 @@ import React, { useContext } from "react"
 import { MovieContext } from "./MovieProvider"
 
 export const MovieSearch = () => {
-    const { searchMovie, filteredMovies } = useContext(MovieContext)
+    const { searchMovie, filteredMovies, setFilteredMovies } = useContext(MovieContext)
   
     return (
       <>
         <div className ="movie__search">Search for a movie:
-        <input type="text"
-          className="input--wide"
-          onKeyUp={(event) => searchMovie(event.target.value)}
-          placeholder="Search for a movie... " />
+          <input type="text"
+            className="input--wide"
+            onKeyUp={(event) => {
+              if (event.target.value !== "") {
+                searchMovie(event.target.value)
+              } else {setFilteredMovies([])}
+            }
+          }
+            placeholder="Search for a movie... " />
         </div>
-        <div>
-            {filteredMovies.map(movie => <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}/>)}
+        <div className="searched__movies">
+            {filteredMovies.map(movie => <img className="filtered__movies" src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}/>)}
         </div>
       </>
     )
